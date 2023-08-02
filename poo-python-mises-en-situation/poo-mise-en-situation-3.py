f8457b96d593c2b05ba3f0b630eccc83c5505c0f
blackboyman <cksuperatif@live.fr>
2023-08-02 16:05:30 +0000

    CorrectionEXO3

--- poo-python-mises-en-situation/poo-mise-en-situation-3.py
+++ poo-python-mises-en-situation/poo-mise-en-situation-3.py
@@ -2,16 +2,19 @@
 
 # ---
 class Chat:
-    def __int__(self):
+    def __init__(self):
         self.nom = "inconnu"
-
-    def SePresenter(self, nom_facultatif=""):
-        self.nom = nom_facultatif
+    
+    def SePresenter(self, nom_facultatif=""):    
+        if nom_facultatif =="":
+            self.nom
+        else:
+            self.nom = nom_facultatif
         print("Bonjour, je suis un chat et je m'appelle " + self.nom)
 
 # ---
 class Personne:
-    def __int__(self, nom: str):
+    def __init__(self, nom: str):
         self.nom = nom
 
     def SePresenter(self):
@@ -21,8 +24,8 @@ class Personne:
 chat1 = Chat()
 chat1.SePresenter()  # Bonjour, je suis un chat et je m'appelle inconnu
 
-chat2 = Chat("Garfield")
-chat2.SePresenter()  # Bonjour, je suis un chat et je m'appelle Garfield
+chat2 = Chat()
+chat2.SePresenter("Garfield")  # Bonjour, je suis un chat et je m'appelle Garfield
 
-personne = Personne("Jean")
-Personne.SePresenter()  # Bonjour, je suis une personne et je m'appelle Jean
+personne1 = Personne("Jean")
+personne1.SePresenter()  # Bonjour, je suis une personne et je m'appelle Jean
